df5eafb49f988d447157ec15e12f19193c60ccdb
sirinekabboura <83769829+sirinekabboura@users.noreply.github.com>
2025-01-08 13:29:59 +0100

    Add files via upload

---
+++ migrations/Version20241122145356.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241122145356 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD date_of_birth DATE DEFAULT NULL, ADD gender VARCHAR(10) DEFAULT NULL, ADD country VARCHAR(100) DEFAULT NULL, ADD postal_code VARCHAR(20) DEFAULT NULL, ADD phone_number VARCHAR(20) DEFAULT NULL, ADD address VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP date_of_birth, DROP gender, DROP country, DROP postal_code, DROP phone_number, DROP address');
+    }
+}
